fdb96813a9c0fda792afbd8ad1641c2760d5eac7
alep-2000 <alessiopiras056@gmail.com>
2023-12-15 16:29:58 +0100

    Create categoria

--- src/main/java/org/lessons/java/controller/CategoriaController.java
+++ src/main/java/org/lessons/java/controller/CategoriaController.java
@@ -7,8 +7,11 @@ import org.lessons.java.db.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.validation.Valid;
@@ -28,18 +31,49 @@ public class CategoriaController {
 		return "categorie";
 	}
 	
+	@GetMapping("/fotos/categorie{id}")
+	public String getCategoria(Model model,
+			@PathVariable int id) {
+		
+		Categoria categoria = categoriaService.findById(id);
+		model.addAttribute("categoria", categoria);
+		
+		return "categorie";
+	}
+	
+	@GetMapping("/fotos/categorie/create")
+	public String createCategorie(Model model) {
+		
+		Categoria categoria = new Categoria();
+		
+		model.addAttribute("categoria", categoria);
+		
+		return "categoria-form";
+	}
+	
+	@PostMapping("/fotos/categorie/create")
+	public String storeCategoria(
+			Model model,
+			@Valid @ModelAttribute Categoria categoria, 
+			BindingResult bindingResult) {
+			
+		
+		return saveCategoria(model, categoria, bindingResult);
+	}
+	
 	private String saveCategoria(Model model,
-			@Valid @ModelAttribute Categoria categoria) {
+			@Valid @ModelAttribute Categoria categoria,
+			BindingResult bindingResult) {
 		
-		System.out.println("Ingrediente:\n" + categoria);
+		System.out.println("Categoria:\n" + categoria);
 		System.out.println("\n---------------\n");
-//		System.out.println("Error:\n" + bindingResult);
+		System.out.println("Error:\n" + bindingResult);
 		
-//		if (bindingResult.hasErrors()) {
-//			
-//			model.addAttribute("ingrediente", ingrediente);
-//			return "ingrediente-form";
-//		}
+		if (bindingResult.hasErrors()) {
+			
+		model.addAttribute("categoria", categoria);
+			return "categoria-form";
+	}
 		
 		categoriaService.save(categoria);
 	
